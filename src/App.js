018f08e1ba582fa4665f7d1cce3a2b523462486f
prashantchavan <prashantchavan312199@gmail.com>
2022-03-17 16:25:57 +0530

    complete project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
+import Todo from './component/Todo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-<h1>hallow now im creating the todo list App</h1>
+      <h1>createning todo </h1>
+      <Todo/>
     </div>
+
   );
 }
 
---
+++ src/component/Todo.js
@@ -0,0 +1,55 @@
+import React,{useState, useRef} from "react";
+
+function Todo(){
+
+    const [todo ,SetTodo] =useState([])
+    const[currentTodo,setcurrentTodo] = useState('')
+
+    const inputval=useRef(null)
+
+    console.log(currentTodo)
+    function AddTask(){
+        SetTodo([...todo,{task:currentTodo,complete:false}])
+        // console.log(event.target)
+
+        inputval.current.value=""
+    }
+
+    const deleteTask=(deleteTaskval)=>{
+        SetTodo(
+            todo.filter((task)=>{
+                return task.task !==deleteTaskval
+            })
+        )
+    }
+    
+    console.log(todo)
+    return (
+        <div className="todo">
+
+            <input type='text' placeholder="Text..."
+            ref={inputval}
+            onChange={(event)=>{
+                setcurrentTodo(event.target.value)
+            }}/>
+            <button
+                onClick={AddTask}
+            > Add me</button>
+            <hr/>
+
+            <div>
+            {todo.map((val,key)=>{
+                return(
+                    <div id="task">
+                        <li key={key}>{val.task}</li>
+                        <button onClick={()=>deleteTask(val.task)} >X</button>
+                    </div>
+                )
+            })}
+
+            </div>
+        </div>
+    )
+}
+
+export default Todo
